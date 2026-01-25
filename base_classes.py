95c7f73215ff2626ca8332e553f03643e70dc834
rekabxam <bakermax1605@gmail.com>
2026-01-25 14:54:35 +1000

    Added init and pull prices method of Holding()

--- base_classes.py
+++ base_classes.py
@@ -1,13 +1,24 @@
-# Import packages
-
 import numpy as np
 import pandas as pd
 import yfinance as yf
 
-# Define key classes
-
 class Holding():
-    pass
+    
+    def __init__(self, ticker:  str, type: str):
+
+        self._tkr = ticker
+        self._type = type
+    
+    def pull_prices(self):
+
+        self._stock = yf.Ticker(self._tkr)
+        self._hist = self._stock.history('max')['Close']
+        
+        return self._hist
+
+    def calc_returns(self):
+        pass
+
 
 class Portfolio():
     pass
