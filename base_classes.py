292ee0098a337e3b2c15b111ad754da8b4118471
rekabxam <bakermax1605@gmail.com>
2026-01-25 14:47:08 +1000

    Added new python file with imports + base class names

---
+++ base_classes.py
@@ -0,0 +1,16 @@
+# Import packages
+
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
+# Define key classes
+
+class Holding():
+    pass
+
+class Portfolio():
+    pass
+
+class Simulation():
+    pass
