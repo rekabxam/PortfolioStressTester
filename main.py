7024fa6e3c1dc6fb889dac9bbc7fdb03f47ebb00
rekabxam <bakermax1605@gmail.com>
2026-02-17 20:55:08 +1000

    main.py: Added basic plan

---
+++ main.py
@@ -0,0 +1,26 @@
+from base_classes import *
+
+# this file will contain the code to run the program
+
+class StressModel():
+    pass
+
+class StressTester():
+    def __init__(self):
+        pass
+
+    def execute():
+        # 1) section of code printing program execute message/design w/ input prompt from model that checks to determine what version to use (imnport/build)
+        # 2) section of code that executes method of model based on whether import/build was given
+        # 3) section of code that prompts user for other specifications
+        # 4) section of code that generates simulation (instantiates object and runs gen_summary)
+        # 5) section of code that prompts whether user wants to 
+        # 6) section of code printing ending message from view class
+        pass
+
+def main():   
+    controller = StressTester()
+    #controller.execute()
+
+if __name__=="__main__":
+    main()
